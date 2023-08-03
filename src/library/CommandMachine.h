14ca64aa1b9b9b7d8e2bed6d548b4407fcf16eca
dilog777 <132608049+dilog777@users.noreply.github.com>
2023-08-03 20:40:43 +0300

    Fix build

--- src/library/CommandMachine.h
+++ src/library/CommandMachine.h
@@ -7,6 +7,8 @@
 class CommandMachine
 {
 public:
+	virtual ~CommandMachine() = default;
+
 	virtual void beginBlock() = 0;
 	virtual void endBlock() = 0;
 	virtual void textCommand(const std::string &text) = 0;
