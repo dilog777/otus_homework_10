23423a7011935a38baa6bcf51aa5f0647f834d7a
dilog777 <132608049+dilog777@users.noreply.github.com>
2023-08-27 14:26:44 +0300

    Added [[maybe_unused]]

--- src/library/AsyncManager.cpp
+++ src/library/AsyncManager.cpp
@@ -10,21 +10,21 @@ AsyncManager &AsyncManager::getInstance()
 
 
 
-AsyncManager::Handle AsyncManager::connect(std::size_t bulk)
+AsyncManager::Handle AsyncManager::connect([[maybe_unused]] std::size_t bulk)
 {
 	return nullptr;
 }
 
 
 
-void AsyncManager::receive(Handle handle, const std::string &buffer)
+void AsyncManager::receive([[maybe_unused]] Handle handle, [[maybe_unused]] const std::string &buffer)
 {
 
 }
 
 
 
-void AsyncManager::disconnect(Handle handle)
+void AsyncManager::disconnect([[maybe_unused]] Handle handle)
 {
 
 }
