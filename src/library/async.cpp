462037c09f7e6bbafadea54b9f1cbff824df37dc
dilog777 <132608049+dilog777@users.noreply.github.com>
2023-08-29 22:13:31 +0300

    Update async.cpp

--- src/library/async.cpp
+++ src/library/async.cpp
@@ -9,24 +9,21 @@ namespace async
 
 handle_t connect(std::size_t bulk)
 {
-	auto &manager = AsyncManager::getInstance();
-	return manager.connect(bulk);
+	return AsyncManager::getInstance().connect(bulk);
 }
 
 
 
 void receive(handle_t handle, const char *data, std::size_t size)
 {
-	auto &manager = AsyncManager::getInstance();
-	manager.receive(handle, std::string(data, size));
+	AsyncManager::getInstance().receive(handle, std::string(data, size));
 }
 
 
 
 void disconnect(handle_t handle)
 {
-	auto &manager = AsyncManager::getInstance();
-	manager.disconnect(handle);
+	AsyncManager::getInstance().disconnect(handle);
 }
 
 }
