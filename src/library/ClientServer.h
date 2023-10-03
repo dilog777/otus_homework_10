cbc5da21610e79d27b022e155cb9224d6a4aa825
dilog777 <132608049+dilog777@users.noreply.github.com>
2023-10-03 20:48:17 +0300

    Update after review

--- src/library/ClientServer.h
+++ src/library/ClientServer.h
@@ -12,6 +12,9 @@ public:
 	ClientServer(const std::shared_ptr<ClientMessageHandler> &messageHandler);
 	~ClientServer();
 
+	ClientServer(ClientServer const &) = delete;
+	ClientServer &operator=(ClientServer const &) = delete;
+
 	using Port = unsigned short;
 	void run(Port port);
 
